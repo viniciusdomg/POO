60c7c9dcfb8d0be495ea19a2e2475f4e37c3f499
Vinicius de Oliveira <130383526+viniciusdomg@users.noreply.github.com>
2023-09-29 09:35:06 -0300

    Criando compra de Ingressos de um cinema

---
+++ lista02/quest04/src/aplicacao/Ingresso.java
@@ -0,0 +1,48 @@
+package aplicacao;
+
+public class Ingresso {
+
+	private int numberId, seat, value;
+	private String cpf, name;
+	
+	public int getNumberId() {
+		return numberId;
+	}
+	public void setNumberId(int numberId) {
+		this.numberId = numberId;
+	}
+	
+	public int getSeat() {
+		return seat;
+	}
+	public void setSeat(int seat) {
+		this.seat = seat;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public float getValue() {
+		return value;
+	}
+	public void setValue(String choose) {
+		if(choose.equals("Meia")) {
+			value = 8;
+		}else if(choose.equals("Inteira")) {
+			value = 16;
+		}
+	}
+	
+	
+}
