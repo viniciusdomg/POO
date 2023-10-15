7ac41f051f59259ae3c17178ef88035c653ea0ea
vinicius <viniciusmgomes2005@hotmail.com>
2023-10-15 17:15:12 -0300

    Create Contato.java

---
+++ lista 02/quest05/src/aplicacao/Contato.java
@@ -0,0 +1,30 @@
+package aplicacao;
+
+public class Contato {
+	private String name, telephone, email;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
